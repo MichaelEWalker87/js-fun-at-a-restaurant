4a3d1471069966b9e5c1585b5140855e486cbcbc
Michael Walker <Michael.e.walker.87@gmail.com>
2020-07-03 10:57:26 -0600

    Completed order test and file

--- src/meal.js
+++ src/meal.js
@@ -1,7 +1,7 @@
 function nameMenuItem(menuItem) {
   return `Delicious ${menuItem}`
 }
-// made funtion added paramitor the return the srting
+// declared funtion added paramitor the return the srting
 
 
 
@@ -13,7 +13,7 @@ function createMenuItem(itemName, price, foodType){
   }
   return menuItem
 }
-// made funtion with 3 paramitors, made an object litteral the holds 3 values
+// declared funtion with 3 paramitors, made an object litteral the holds 3 values
 //returned the object literal
 
 function addIngredients(ingredient1, ingredient2){
@@ -21,21 +21,26 @@ function addIngredients(ingredient1, ingredient2){
       ingredient2.push(ingredient1);
     }
 }
-/* made funtrion addIngredients added 2 paramitors
+/* declared function addIngredients added 2 paramitors
   used the push to add the ingredient1 to the array ingredient2
+  used an if statment and the bang operator to say if
+  includes1 array element does not equal anything
+  in array ingredient2 then push the element to the array
+  ingredient2
 */
 
 function formatPrice(initialPrice){
     return `\$${initialPrice}`
 }
-/*made a function formatPrice added a paramitor
-returned paramitor with $ added to number
+/*declared a function formatPrice added a paramitor
+returned string paramitor with $ added to number
 */
 //note to self: remember to go slow like Orlando said
 
 function decreasePrice(discount){
   return discount * .9
 }
+//declared a funtion add paramitor that multiples by .9
 
 function createRecipe(title, ingredients , menuItemType){
   var brunch = {
--- src/order.js
+++ src/order.js
@@ -1,7 +1,72 @@
+function takeOrder(obj, deliveryOrders){
+ if (deliveryOrders.length < 3){
+  deliveryOrders.push(obj);
+}
+}
+/* declared a function takeOrder and added 2 paramitors
+added if statment that takes deliveryOrders.length
+and if it is less than 3 then it pushs obj to the
+deliveryOrders
+*/
+
+
+function refundOrder(removeItem, deliveryOrders){
+  var orderNumberArray = removeItem -1
+
+  deliveryOrders.splice(orderNumberArray, 1);
+
+}
+/* decleared refundOrder with 2 paramitors
+decleared var and set it to the order number minus 1
+then placed that var orderNumberArray to index what spot
+to splice
+*/
+
+function listItems(orderObt){
+  var itemArray = []
+  for (i = 0; i < orderObt.length; i++){
+  itemArray.push(orderObt[i].item)
+  }
+  return itemArray.join(', ');
+
+}
+//pseudocode for the problem above
+//need to return the objects item value
+//need to be returned as a string
+//is accessing the array elements with for loop
+//access the item value and push to new var itemArray
+// turn item value into string with , using join() method
+
+function searchOrder(items, checkItem){
+  var itemArray2 = []
+  var ifRight = false;
+
+  for (i = 0; i < items.length; i++){
+    itemArray2.push(items[i].item)
+  }
+  for (m = 0; m < itemArray2.length; m++){
+    if (itemArray2[m] === checkItem){
+      ifRight = true;
+    }
+  }
+  return ifRight;
+}
+
+//pseudocode for the problem above
+//should be able to return item in odject array
+// var deliveryOrders passes orders
+//should return true if the string in any obj === order item
+//the code need to take 2 paramitor the object and item checked
+// need to extract the item from any object in the array
+//need to compaire item.item to checkItem
+// IF they are === then return true ELSE false
+//Persoanl note pseudocode is the BEST!
+
+
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+  takeOrder,
+  refundOrder,
+  listItems,
+  searchOrder
 }
