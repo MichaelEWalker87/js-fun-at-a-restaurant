8afb25f7bc8a07213fc67842ad21700a76c543e3
Michael Walker <Michael.e.walker.87@gmail.com>
2020-07-04 11:48:27 -0600

    Refactored removeMenuItem

--- src/restaurant.js
+++ src/restaurant.js
@@ -55,44 +55,63 @@ function addMenuItem(rest, item){
 //
 
 
-|
 
 
 
-// function removeMenuItem(rest2, item2, meal){
-//   if (meal === undefined){
-//     meal = 'dinner'
-//   }
-//
-//   if (rest2.menus[meal].includes(item2)){
-//     rest2.menus[meal].splice(item2, 1);
-//   return `No one is eating our ${item2} \- it has been removed from the ${meal} menu\!`
-//   } else {
-//     return `Sorry, we don't sell ${item2}, try adding a new recipe\!`
-//   }
-// }
+function removeMenuItem(rest2, item2, meal = 'dinner'){
+  var pos
+  for (var i = 0; i < rest2.menus[meal].length; i++){
+    if (rest2.menus[meal][i].name === item2){
+      pos = i
+    }
+  }
+  if (pos !== undefined){
+      rest2.menus[meal].splice(pos, 1);
+      return `No one is eating our ${item2} \- it has been removed from the ${meal} menu\!`
+  } else {
+      return `Sorry, we don't sell ${item2}, try adding a new recipe\!`
+  }
+}
 
+
+// decleared a funtion set to 3 paramitors where meal if undefined is set to dinner
+//declared var pos and left undefined
+//ran a for loop the run trhough breakfast lunch or dinner array
+//if at any time the name at value is === item2 then set pos to the value of i
+    //orginally this had 2 var one for i and on to change to true but I was able to make pos byitself work
+//in my next block of code
+// if pos does not equal undefined then
+// splice at postion of pos number which will be the number of index in array remove one item
+//then return string
+//if pos is undefined then it will return the other string  
+
+
+
+
+
+
+//I wanted to walk thorugh my thought process unfortunatly I lost some of my eailer
+// work on this.
+//I started useing the .includes to get things going but includes but I was not pulling the right
+// index I was able to get 2 test to pass but not all 3 at once
+// then as seen in the code below I did an if meal is undefined which it is then
+//return `Sorry, we don't sell ${item2}, try adding a new recipe\!`
+// as seen with the code below which will work but it was not realy what I wanted
+// I did som refactoring and ended with the code above
 
 
 // function removeMenuItem(rest2, item2, meal){
 //   for (var i = 0; i < rest2.menus[meal].length; i++){
-//   if (meal === undefined){
-//     return `Sorry, we don't sell ${item2}, try adding a new recipe\!`
-//   }else
-//     (rest2.menus[meal][i] === item2){
-//     rest2.menus[meal].splice(item2, 1);
-//     return `No one is eating our ${item2} \- it has been removed from the ${meal} menu\!`
-//   }//else{
-//     //return `Sorry, we don't sell ${item2}, try adding a new recipe\!`
+//     if (meal === undefined){
+//       return `Sorry, we don't sell ${item2}, try adding a new recipe\!`
+//     }else if (rest2.menus[meal][i] === item2) {
+//       rest2.menus[meal].splice(item2, 1);
+//       return `No one is eating our ${item2} \- it has been removed from the ${meal} menu\!`
+//     }
 //   }
 // }
-//
-// }
 
-//if (rest2.menus[meal].includes(item2)){
 
-//think i will use the splice method to remove an item
-//
 
 module.exports = {
   createRestaurant,
